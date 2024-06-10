e3a8f037413fb3726acfe40435f338b1185bd0a0
JooYoo <jooyoo@outlook.com>
2024-06-10 18:27:51 +0200

    feat: init ios project

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Yu on 2024-06-10.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
